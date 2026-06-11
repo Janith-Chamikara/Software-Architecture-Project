b469e75d3adda35b1160c37dfba15874f45777da
Janith-Chamikara <janithchamikara13@gmail.com>
2026-06-12 01:11:05 +0530

    Added auth module with JWT token based security'

---
+++ backend/src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
+
+dotenv.config({
+  path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env',
+});
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: '*',
+  });
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+
+  if (process.env.NODE_ENV === 'production') {
+    console.log(`Running in production mode on port ${port}`);
+  } else {
+    console.log(`Running in development mode on port ${port}`);
+  }
+}
+bootstrap();
---
+++ backend/src/user/services/user.service.ts
@@ -0,0 +1,118 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AccountSetupDto } from '../dto/user-account-setup.dto';
+import { User } from '@clerk/backend';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async setAccount(accountSetupDto: AccountSetupDto, currentUser: User) {
+    console.log(accountSetupDto);
+    if (accountSetupDto.usage === 'PERSONAL') {
+      const jobField = accountSetupDto?.jobField;
+      currentUser.job = jobField;
+      await this.updateUserById(currentUser.id, currentUser);
+    } else {
+      const companyName = accountSetupDto.companyName;
+      const industryType = accountSetupDto.industryType;
+      const numberOfEmployees = accountSetupDto.numberOfEmployees;
+
+      await this.updateUserById(currentUser.id, {
+        usage: 'business',
+        companyName,
+        industryType,
+        numberOfEmployees,
+      });
+    }
+    //workspace
+    const workspaceName = accountSetupDto.workspace.name;
+    const workspaceDetails = accountSetupDto.workspace.description;
+    const workspace = await this.workspaceService.createWorkspace({
+      name: workspaceName,
+      description: workspaceDetails,
+      ownerId: currentUser.id,
+    });
+    currentUser.isInitialSetupCompleted = true;
+    await this.updateUserById(currentUser.id, {
+      isInitialSetupCompleted: true,
+    });
+    return {
+      workspace,
+      message: 'Congratulations! Your account has been successfully set up.',
+    };
+  }
+
+  async getUserById(id: string) {
+    const isUserExists = await this.prismaService.user.findUnique({
+      where: { id: id },
+    });
+    if (!isUserExists) {
+      throw new BadRequestException('User with the provided id does not exist');
+    }
+    return isUserExists;
+  }
+
+  async updateUserById(id: string, updatedUserContent: Partial<User>) {
+    const isUserExists = await this.getUserById(id);
+    if (!isUserExists) {
+      throw new NotFoundException('User not found');
+    }
+    const updatedUser = await this.prismaService.user.update({
+      where: {
+        id: isUserExists.id,
+      },
+      data: updatedUserContent,
+    });
+
+    if (!updatedUser) {
+      throw new InternalServerErrorException(
+        'Error occurred during updating user',
+      );
+    }
+    return updatedUser;
+  }
+
+  async deleteUserById(userId: string) {
+    if (!userId) {
+      throw new NotFoundException('Cannot find user Id');
+    }
+
+    const workspaces = await this.prismaService.workspace.findMany({
+      where: {
+        ownerId: userId,
+      },
+    });
+
+    if (workspaces.length > 0) {
+      await this.prismaService.testimonial.deleteMany({
+        where: {
+          workspaceId: {
+            in: workspaces.map((workspace) => workspace.id),
+          },
+        },
+      });
+    }
+
+    await this.prismaService.workspace.deleteMany({
+      where: {
+        ownerId: userId,
+      },
+    });
+
+    return await this.prismaService.user.delete({
+      where: {
+        id: userId,
+      },
+    });
+  }
+
+  async getAllUsers() {
+    return this.prismaService.user.findMany();
+  }
+}
